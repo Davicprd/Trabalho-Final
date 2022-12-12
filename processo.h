19c0a050bc1793697c2b4de4df09d924f6be3beb
Francisco Davi Cassundé Prado <113373345+Davicprd@users.noreply.github.com>
2022-12-11 23:19:35 -0300

    processo.h

--- processo.h
+++ processo.h
@@ -5,6 +5,15 @@
 #include <string.h>
 #define QTDIMG 1312
 
+FILE *f;
+DIR *d;
+clock_t begin, end;
+struct dirent *dir;
+struct pgm img, img2;
+int quant;
+char name[50], a_name[10];
+double time_per_img, time_total;
+
 struct pgm
 {
     int tipo;
