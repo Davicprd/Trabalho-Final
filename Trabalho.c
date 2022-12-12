dddefe96363348f7fd154a72dd63f45daf14fe65
Francisco Davi Cassundé Prado <113373345+Davicprd@users.noreply.github.com>
2022-12-11 23:17:34 -0300

    Trabalho.c

--- Trabalho.c
+++ Trabalho.c
@@ -1,26 +1,12 @@
-#include <dirent.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
 #include "processo.h"
 
-#define QTDIMG 1310
-
-FILE *f;
-DIR *d;
-char name[30] = "./oncotex_pgm/";
 int main(void)
 {
-
-    clock_t begin, end;
-    double time_per_img, time_total = 0;
-    struct dirent *dir;
-    struct pgm img, img2;
-    int quant;
+    strcpy(name,"./oncotex_pgm/");
     // Aqui nos solicitamos a quantização em N níveis para o usuário
-    puts("Digite o número de niveis de quantização: (8/16/32/64/128/256)");
+    puts("Digite o nível de quantização: ");
     scanf("%i", &quant);
+    sprintf(a_name, "%i-scm.txt", quant);
     // Aqui nós começamos a contagem
     begin = clock();
     // Aqui nós abrimos a pasta contento os arquivos
@@ -33,31 +19,25 @@ int main(void)
         dir = readdir(d);
         dir = readdir(d);
         // Aqui inicializamos o Arquivo de texto para captura de dados dos vetores SCM
-        f = fopen("SCM.txt", "w+");
-        int cont = 0;
+        f = fopen(a_name, "w+");
         // Aqui nos usamos um FOR para realizar todos os processos para cada duas imagens(sem filtro e com filtro)
-        for (int i = 0; i <= (QTDIMG / 2); i++){
-        strcpy(name,"./oncotex_pgm/");
-        dir = readdir(d);
-        strcat(name,dir->d_name);
-        readPGMImage(&img,name);
-        strcpy(name,"./oncotex_pgm/");
-        dir = readdir(d);
-        strcat(name,dir->d_name);
-        readPGMImage(&img2,name);
-        ProcessoPGMImage(&img, &img2, dir->d_name, quant,f);
-        printf("\n%i",cont++);
+        for (int i = 1; i <= (QTDIMG / 2); i++)
+        {
+            strcpy(name, "./oncotex_pgm/");
+            dir = readdir(d);
+            strcat(name, dir->d_name);
+            LerIMG(&img, name);
+            strcpy(name, "./oncotex_pgm/");
+            dir = readdir(d);
+            strcat(name, dir->d_name);
+            LerIMG(&img2, name);
+            Processo(&img, &img2, dir->d_name, quant, f);
         }
 
         // Aqui nós fechamos o arquivo e o diretório
         fclose(f);
         closedir(d);
     }
-    else
-    {
-        printf("Nem abre");
-    }
-
     // Finalizamos a contagem
     end = clock();
 
@@ -66,7 +46,7 @@ int main(void)
     // Armazenando o tempo total gasto nos processos
     time_total += time_per_img;
 
-    printf("\nTempo médio: %lf\n", time_total / QTDIMG);
+    printf("\nTempo Médio: %lf\n", time_total / QTDIMG);
     printf("\nTempo Total: %lf\n", time_total);
     return 0;
 }
