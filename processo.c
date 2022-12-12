e82e235e0c5929bd003ea79aaeb8f4a3716576df
leonardobmt1205 <120220940+leonardobmt1205@users.noreply.github.com>
2022-12-12 19:26:34 -0300

    processo.c v1.0

--- processo.c
+++ processo.c
@@ -1,3 +1,11 @@
+//*******************************************************************//
+//* Alunos: Allan Neil, Francisco Davi, Leonardo Borges            *//
+//* Matriculas: 20221045050028, 20221045050125, 20221045050230    *//
+//* Avaliação 04: Trabalho Final                                 *//
+//* 04.505.23 - 2022.2 - Prof. Daniel Ferreira                  *//
+//* Compilador gcc Versão 6.3.0                                *//
+//*************************************************************//
+
 #include "processo.h"
 
 void LerIMG(struct pgm *img, char *filename)
@@ -5,12 +13,13 @@ void LerIMG(struct pgm *img, char *filename)
     FILE *fp;
     char ch;
 
+    //Aqui foi conferido se existe algum arquivo txt para ser armazenado os vetores concatenados.
     if (!(fp = fopen(filename, "r")))
     {
         perror("Erro.");
         exit(1);
     }
-
+    //Aqui é conferido se a imagem possui uma tipagem pgm.
     if ((ch = getc(fp)) != 'P')
     {
         puts("A imagem fornecida não está no formato pgm");
@@ -18,39 +27,44 @@ void LerIMG(struct pgm *img, char *filename)
     }
 
     img->tipo = getc(fp) - 48;
-
+    
     fseek(fp, 1, SEEK_CUR);
-
+    //Aqui é utlizado um "while" para não contabilizar a linha de comentário, presente em algumas imagens.
     while ((ch = getc(fp)) == '#')
-    {
+    {     
         while ((ch = getc(fp)) != '\n')
             ;
     }
-
+    
     fseek(fp, -2, SEEK_CUR);
-
+    //Aqui são salvas as dimensões da imagem processada.
     fscanf(fp, "%d %d", &img->i, &img->j);
+    //Verifica o indicador de erro em fp.
     if (ferror(fp))
     {
         perror(NULL);
         exit(3);
     }
+    //Aqui é salvo o nível de quantização escolhido.
     fscanf(fp, "%d", &img->quan);
     fseek(fp, 0, SEEK_CUR);
 
     img->pData = (unsigned char *)malloc(img->j * img->i * sizeof(unsigned char));
-
+//Aqui o switch case analisa o tipo da imagem, para definir a maneira que matriz vai ser formada.
     switch (img->tipo)
     {
+    //Caso seja textual(P2):  
     case 2:
         for (int k = 0; k < (img->j * img->i); k++)
         {
             fscanf(fp, "%hhu", img->pData + k);
         }
         break;
+    //Caso seja binária(P5):   
     case 5:
         fread(img->pData, sizeof(unsigned char), img->j * img->i, fp);
         break;
+    //Caso não atenda nenhuma das situações:  
     default:
         puts("Não está implementado");
     }
